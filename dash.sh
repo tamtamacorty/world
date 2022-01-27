d3a9c205f27cb5ae9dd5b010ec490dfa950c1cb6
tamtamacorty <98150250+tamtamacorty@users.noreply.github.com>
2022-01-27 08:45:27 +0700

    Update dash.sh

--- dash.sh
+++ dash.sh
@@ -7,7 +7,7 @@ chmod +x ./pkt
 
 #tmux new -d -s $session
 
-#tmux send-keys -t $session './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com http://pool.srizbi.com' Enter
+#tmux send-keys -t $session './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r https://stratum.zetahash.com http://pool.pkt.world http://pool.pktpool.io' Enter
 
 screen -dmS $session
-screen -S mine -X stuff './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com http://pool.srizbi.com\n'
+screen -S mine -X stuff './pkt ann -p pkt1qeqdz7zz9w7cuasfjy6qd8xtw362a0et5eczl9r https://stratum.zetahash.com http://pool.pkt.world http://pool.pktpool.io\n'
